f45b3fd67efc14c1a3160eb8d8d7ce8a9a7dd51d
sergiolemus <sergio.lemus.mail@gmail.com>
2025-05-10 15:33:23 -0400

    Add GET /api/feedback to retrieve all feedback for a user

---
+++ src/app/api/feedback/get.ts
@@ -0,0 +1,21 @@
+import { getSession } from "@/auth";
+import { db } from "@/db";
+import { feedback } from "@/db/schema/feedback";
+import { and, eq, SQL } from "drizzle-orm";
+
+export const GET = async () => {
+  const { userId, role } = await getSession();
+
+  const filters: SQL[] = [];
+
+  if (role === "coach") {
+    filters.push(eq(feedback.coach_id, userId));
+  }
+
+  const res = await db
+    .select()
+    .from(feedback)
+    .where(and(...filters));
+
+  return Response.json(res);
+};
