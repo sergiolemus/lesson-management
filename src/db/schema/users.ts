db66e963fd6618586229956cfcd0837ed88bd5bd
sergiolemus <sergio.lemus.mail@gmail.com>
2025-05-09 20:11:09 -0400

    Add Drizzle ORM config + base users table

---
+++ src/db/schema/users.ts
@@ -0,0 +1,12 @@
+import { sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const users = sqliteTable("user", {
+  id: text("id")
+    .primaryKey()
+    .$defaultFn(() => crypto.randomUUID()),
+  name: text("name"),
+  email: text("email").unique(),
+});
+
+export type InsertUser = typeof users.$inferInsert;
+export type SelectUser = typeof users.$inferSelect;
