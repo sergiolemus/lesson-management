061fa5cef133a2e8995113e75e3618be8e353729
sergiolemus <sergio.lemus.mail@gmail.com>
2025-05-09 23:06:36 -0400

    Add GET /api/users/:id route to retrieve specific user data post selection

---
+++ src/app/api/users/[id]/get.ts
@@ -0,0 +1,14 @@
+import { db } from "@/db";
+import { users } from "@/db/schema/users";
+import { eq } from "drizzle-orm";
+import { NextRequest } from "next/server";
+
+export const GET = async (
+  _request: NextRequest,
+  { params }: { params: Promise<{ id: string }> }
+) => {
+  const { id } = await params;
+  const results = await db.select().from(users).where(eq(users.id, id));
+
+  return Response.json(results);
+};
