074065ca733c239bef4ab920fc9f57f15425d822
sergiolemus <sergio.lemus.mail@gmail.com>
2025-05-10 12:08:14 -0400

    Update GET /api/slots/:coach_id to filter to all dates lte end_date query

--- src/app/api/slots/[coach_id]/get.ts
+++ src/app/api/slots/[coach_id]/get.ts
@@ -1,6 +1,6 @@
 import { db } from "@/db";
 import { slots } from "@/db/schema/slots";
-import { and, eq, gte, SQL } from "drizzle-orm";
+import { and, eq, gte, lte, SQL } from "drizzle-orm";
 import { NextRequest } from "next/server";
 
 export const GET = async (
@@ -11,11 +11,16 @@ export const GET = async (
   const { searchParams } = request.nextUrl;
 
   const start_date = searchParams.get("start_date");
+  const end_date = searchParams.get("end_date");
 
   const filters: SQL[] = [eq(slots.coach_id, coach_id)];
 
   if (start_date) {
     filters.push(gte(slots.start_date, Number(start_date)));
+  }
+
+  if (end_date) {
+    filters.push(lte(slots.end_date, Number(end_date)));
   }
 
   const results = await db
