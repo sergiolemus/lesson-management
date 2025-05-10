3fe44c92c769686a67883b31b1a79bb4b7e367f7
sergiolemus <sergio.lemus.mail@gmail.com>
2025-05-10 11:53:15 -0400

    Simplify GET /api/users

--- src/app/api/users/get.ts
+++ src/app/api/users/get.ts
@@ -1,24 +1,26 @@
 import { db } from "@/db";
 import { users } from "@/db/schema/users";
-import { eq } from "drizzle-orm";
+import { and, eq, SQL } from "drizzle-orm";
 import { NextRequest } from "next/server";
 
 export const GET = async (request: NextRequest) => {
   const { searchParams } = request.nextUrl;
   const role = searchParams.get("role");
 
-  if (!role) {
-    const results = await db
-      .select({ name: users.name, id: users.id, role: users.role })
-      .from(users);
+  const filters: SQL[] = [];
 
-    return Response.json(results);
+  if (role) {
+    filters.push(eq(users.role, role));
   }
 
-  const results = await db
-    .select({ name: users.name, id: users.id, role: users.role })
+  const res = await db
+    .select({
+      name: users.name,
+      id: users.id,
+      role: users.role,
+    })
     .from(users)
-    .where(eq(users.role, role));
+    .where(and(...filters));
 
-  return Response.json(results);
+  return Response.json(res);
 };
