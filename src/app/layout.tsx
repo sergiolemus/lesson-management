6483235cf7ec366e02eae9525252542cd845e81a
sergiolemus <sergio.lemus.mail@gmail.com>
2025-05-11 19:06:51 -0400

    Add base MUI fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,11 @@ import InitColorSchemeScript from "@mui/material/InitColorSchemeScript";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" suppressHydrationWarning>
