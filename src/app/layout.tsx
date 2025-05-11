4fc5b2a59310c00411cd93609724405a9fbab74b
sergiolemus <sergio.lemus.mail@gmail.com>
2025-05-11 13:34:08 -0400

    Add localization provider for MUI date pickers to function

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import React from "react";
 import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "@/theme";
 import InitColorSchemeScript from "@mui/material/InitColorSchemeScript";
+import { LocalizationProvider } from "@mui/x-date-pickers";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
@@ -13,7 +17,9 @@ export default function RootLayout(props: { children: React.ReactNode }) {
         <AppRouterCacheProvider options={{ enableCssLayer: true }}>
           <ThemeProvider theme={theme}>
             <CssBaseline />
-            {props.children}
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+              {props.children}
+            </LocalizationProvider>
           </ThemeProvider>
         </AppRouterCacheProvider>
       </body>
