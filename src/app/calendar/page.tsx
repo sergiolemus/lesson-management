37e4d71f04ed2ea51702cf22ee56ce3c1bc7e1f5
sergiolemus <sergio.lemus.mail@gmail.com>
2025-05-11 09:02:07 -0400

    Structure base view for schedule on /calendar

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,20 +1,7 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import {
-  Box,
-  Typography,
-  Container,
-  Button,
-  Card,
-  CardContent,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-  SelectChangeEvent,
-} from "@mui/material";
-import Cookies from "js-cookie";
+import { Box, Typography, Container, Card, CardContent } from "@mui/material";
 
 export default function Home() {
   const [role, setRole] = useState("coach");
@@ -55,122 +42,56 @@ export default function Home() {
       <Box
         sx={{
           display: "flex",
-          flexDirection: "row",
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
           minHeight: "100vh",
           gap: 2,
         }}
       >
-        <Card variant="outlined" sx={{ width: "400px", padding: "16px" }}>
+        <Card variant="outlined" sx={{ maxWidth: "100%", padding: "16px" }}>
           <CardContent>
             <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
-              Calendar
+              Schedule
             </Typography>
             <Box
-              component="form"
-              onSubmit={handleSubmit}
-              noValidate
               sx={{
                 display: "flex",
-                flexDirection: "column",
-                width: "100%",
+                flexDirection: "row",
+                justifyContent: "center",
+                alignItems: "center",
+                height: "512px",
                 gap: 2,
               }}
             >
-              <FormControl>
-                <InputLabel htmlFor="role">Select Role</InputLabel>
-                <Select
-                  id="role"
-                  name="role"
-                  value={role}
-                  label="Select Role"
-                  onChange={handleRoleChange}
-                >
-                  <MenuItem value={"coach"}>Coach</MenuItem>
-                  <MenuItem value={"student"}>Student</MenuItem>
-                </Select>
-              </FormControl>
-              <FormControl>
-                <InputLabel htmlFor="id">Select User</InputLabel>
-                <Select
-                  id="id"
-                  name="id"
-                  value={id}
-                  label="Select User"
-                  onChange={handleUserChange}
-                >
-                  {users.map(({ id, name }) => (
-                    <MenuItem key={id} value={id}>
-                      {name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                disabled={id === ""}
-              >
-                Sign in
-              </Button>
-            </Box>
-          </CardContent>
-        </Card>
-        <Card variant="outlined" sx={{ width: "400px", padding: "16px" }}>
-          <CardContent>
-            <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
-              Calendar
-            </Typography>
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              noValidate
-              sx={{
-                display: "flex",
-                flexDirection: "column",
-                width: "100%",
-                gap: 2,
-              }}
-            >
-              <FormControl>
-                <InputLabel htmlFor="role">Select Role</InputLabel>
-                <Select
-                  id="role"
-                  name="role"
-                  value={role}
-                  label="Select Role"
-                  onChange={handleRoleChange}
-                >
-                  <MenuItem value={"coach"}>Coach</MenuItem>
-                  <MenuItem value={"student"}>Student</MenuItem>
-                </Select>
-              </FormControl>
-              <FormControl>
-                <InputLabel htmlFor="id">Select User</InputLabel>
-                <Select
-                  id="id"
-                  name="id"
-                  value={id}
-                  label="Select User"
-                  onChange={handleUserChange}
-                >
-                  {users.map(({ id, name }) => (
-                    <MenuItem key={id} value={id}>
-                      {name}
-                    </MenuItem>
-                  ))}
-                </Select>
-              </FormControl>
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                disabled={id === ""}
-              >
-                Sign in
-              </Button>
+              <Card
+                variant="outlined"
+                sx={{ width: "256px", padding: "16px" }}
+              ></Card>
+              <Card
+                variant="outlined"
+                sx={{ width: "256px", padding: "16px" }}
+              ></Card>
+              <Card
+                variant="outlined"
+                sx={{ width: "256px", padding: "16px" }}
+              ></Card>
+              <Card
+                variant="outlined"
+                sx={{ width: "256px", padding: "16px" }}
+              ></Card>
+              <Card
+                variant="outlined"
+                sx={{ width: "256px", padding: "16px" }}
+              ></Card>
+              <Card
+                variant="outlined"
+                sx={{ width: "256px", padding: "16px" }}
+              ></Card>
+              <Card
+                variant="outlined"
+                sx={{ width: "256px", padding: "16px" }}
+              ></Card>
             </Box>
           </CardContent>
         </Card>
