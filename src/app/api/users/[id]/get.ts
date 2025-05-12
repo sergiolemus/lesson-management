4c19065df9422c598ce6c2a2152438e08d135b39
sergiolemus <sergio.lemus.mail@gmail.com>
2025-05-11 22:15:57 -0400

    Update GET /api/users/id for coaches to have access to student info

--- src/app/api/users/[id]/get.ts
+++ src/app/api/users/[id]/get.ts
@@ -1,17 +1,28 @@
 import { getSession } from "@/auth";
 import { db } from "@/db";
 import { users } from "@/db/schema/users";
-import { eq } from "drizzle-orm";
+import { and, eq, SQL } from "drizzle-orm";
 import { NextRequest } from "next/server";
 
 export const GET = async (
   _request: NextRequest,
   { params }: { params: Promise<{ id: string }> }
 ) => {
-  const { id: _id } = await params;
-  const { userId } = await getSession();
+  const { id } = await params;
+  const { userId, role } = await getSession();
 
-  const results = await db.select().from(users).where(eq(users.id, userId));
+  const filters: SQL[] = [];
 
-  return Response.json(results);
+  if (role === "coach" && id) {
+    filters.push(eq(users.id, id));
+  } else {
+    filters.push(eq(users.id, userId));
+  }
+
+  const results = await db
+    .select()
+    .from(users)
+    .where(and(...filters));
+
+  return Response.json(results[0]);
 };
