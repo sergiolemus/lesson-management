2eba3775fcdbb06aa00748aec94ede3f7d2d93d1
sergiolemus <sergio.lemus.mail@gmail.com>
2025-05-12 02:46:09 -0400

    Update POST /api/slots to prevent users from booking overlapping slots

--- src/app/api/slots/post.ts
+++ src/app/api/slots/post.ts
@@ -1,6 +1,7 @@
 import { getSession } from "@/auth";
 import { db } from "@/db";
 import { slots } from "@/db/schema/slots";
+import { and, eq, gt, gte, lt, or } from "drizzle-orm";
 import { NextRequest } from "next/server";
 
 export const POST = async (request: NextRequest) => {
@@ -12,7 +13,33 @@ export const POST = async (request: NextRequest) => {
 
   const { start_date, end_date, coach_id: _coach_id } = await request.json();
 
-  const entry = await db
+  const check = await db
+    .select()
+    .from(slots)
+    .where(
+      and(
+        eq(slots.coach_id, userId),
+        or(
+          and(gt(slots.end_date, start_date), lt(slots.end_date, end_date)),
+          and(eq(slots.start_date, start_date), eq(slots.end_date, end_date)),
+          and(gt(slots.start_date, start_date), lt(slots.start_date, end_date))
+        )
+      )
+    );
+
+  if (check.length > 0) {
+    return Response.json(
+      {
+        error: {
+          code: "CONFLICT",
+          message: "The resource conflicts with an existing one.",
+        },
+      },
+      { status: 409 }
+    );
+  }
+
+  const [entry] = await db
     .insert(slots)
     .values({
       coach_id: userId,
@@ -22,5 +49,5 @@ export const POST = async (request: NextRequest) => {
     })
     .returning();
 
-  return Response.json(entry[0]);
+  return Response.json(entry);
 };
