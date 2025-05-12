d19abb4b2eda590ab2b5e862c37688a421cbf406
sergiolemus <sergio.lemus.mail@gmail.com>
2025-05-11 22:16:43 -0400

    Add <Slot/> for /calendar pages to have feedback modals per booked slots

---
+++ src/app/calendar/_components/Slot.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import React, { useState } from "react";
+import dayjs from "dayjs";
+import { Box, Button, Modal } from "@mui/material";
+import { SlotInfo } from "./SlotInfo";
+import { Student } from "@/lib/types/Student";
+
+export const Slot: React.FC<{
+  id: string;
+  startDate: number;
+  booked: number;
+  coachId: string;
+  studentId: string;
+  onReserve?: () => void;
+}> = ({ id, startDate, booked, studentId }) => {
+  const [openModal, setOpenModal] = useState(false);
+
+  const [student, setStudent] = useState<Student>({
+    name: "",
+    phone_number: "",
+  });
+
+  const handleOpen = async () => {
+    const res = await fetch(`/api/users/${studentId}`, { method: "GET" });
+    const student = await res.json();
+
+    setStudent(student);
+    setOpenModal(true);
+  };
+
+  const handleClose = () => setOpenModal(false);
+
+  const date = dayjs.unix(startDate);
+  const past = date.isBefore(dayjs());
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setOpenModal(false);
+  };
+
+  if (booked) {
+    return (
+      <>
+        <Button
+          key={id}
+          variant="outlined"
+          color="success"
+          disabled={past}
+          onClick={handleOpen}
+        >
+          {dayjs.unix(Number(startDate)).format("hh:mm A")}
+        </Button>
+        <Modal
+          open={openModal}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description"
+        >
+          <Box
+            sx={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+              width: 400,
+              bgcolor: "background.paper",
+              border: "2px solid #000",
+              boxShadow: 24,
+              p: 4,
+            }}
+          >
+            <SlotInfo
+              title="Feedback"
+              date={date}
+              studentName={student.name}
+              phoneNumber={student.phone_number}
+            />
+            <Box component="form" onSubmit={handleSubmit} sx={{ mt: 3 }}>
+              <Button type="submit" fullWidth variant="contained">
+                Submit
+              </Button>
+            </Box>
+          </Box>
+        </Modal>
+      </>
+    );
+  }
+
+  return (
+    <Button key={id} disabled={past} variant="outlined" onClick={handleOpen}>
+      {dayjs.unix(Number(startDate)).format("hh:mm A")}
+    </Button>
+  );
+};
---
+++ src/app/calendar/_components/SlotInfo.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import { Dayjs } from "dayjs";
+import { Box, Typography } from "@mui/material";
+import { formatPhoneNumber } from "@/lib/phone/format";
+
+export const SlotInfo: React.FC<{
+  title: string;
+  studentName: string;
+  date: Dayjs;
+  phoneNumber: string;
+}> = ({ title, studentName, date, phoneNumber }) => {
+  return (
+    <>
+      <Typography
+        id="modal-modal-title"
+        variant="h6"
+        component="h2"
+        sx={{ mb: 2 }}
+      >
+        {title}
+      </Typography>
+      <Box sx={{ display: "flex", flexDirection: "column", gap: 1 }}>
+        <Box
+          id="modal-modal-coach"
+          sx={{ display: "flex", flexDirection: "row" }}
+        >
+          <Typography fontWeight="bold">Student:</Typography>
+          &nbsp;
+          <Typography>{studentName}</Typography>
+        </Box>
+        <Box
+          id="modal-modal-coach"
+          sx={{ display: "flex", flexDirection: "row" }}
+        >
+          <Typography fontWeight="bold">Phone:</Typography>
+          &nbsp;
+          <Typography>{formatPhoneNumber(phoneNumber)}</Typography>
+        </Box>
+        <Box
+          id="modal-modal-date"
+          sx={{ display: "flex", flexDirection: "row" }}
+        >
+          <Typography fontWeight="bold">Date:</Typography>
+          &nbsp;
+          <Typography>{date.format("LL")}</Typography>
+        </Box>
+        <Box
+          id="modal-modal-time"
+          sx={{ display: "flex", flexDirection: "row" }}
+        >
+          <Typography fontWeight="bold">Time:</Typography>
+          &nbsp;
+          <Typography>{`${date.format("LT")} - ${date
+            .add(2, "h")
+            .format("LT")}`}</Typography>
+        </Box>
+      </Box>
+    </>
+  );
+};
