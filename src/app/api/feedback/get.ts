bea319ee09ec6468e0d93ed79c2601cbf4647e5b
sergiolemus <sergio.lemus.mail@gmail.com>
2025-05-12 01:20:45 -0400

    Update GET /api/feedback to filter by lesson_id

--- src/app/api/feedback/get.ts
+++ src/app/api/feedback/get.ts
@@ -2,9 +2,13 @@ import { getSession } from "@/auth";
 import { db } from "@/db";
 import { feedback } from "@/db/schema/feedback";
 import { and, eq, SQL } from "drizzle-orm";
+import { NextRequest } from "next/server";
 
-export const GET = async () => {
+export const GET = async (request: NextRequest) => {
   const { userId, role } = await getSession();
+  const { searchParams } = request.nextUrl;
+
+  const lesson_id = searchParams.get("lesson_id");
 
   const filters: SQL[] = [];
 
@@ -14,6 +18,10 @@ export const GET = async () => {
 
   if (role === "coach") {
     filters.push(eq(feedback.coach_id, userId));
+  }
+
+  if (lesson_id) {
+    filters.push(eq(feedback.lesson_id, lesson_id));
   }
 
   const res = await db
