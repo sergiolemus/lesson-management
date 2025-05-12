bba8deea50d79aaa79c7a438560a0bee5a1f9d37
sergiolemus <sergio.lemus.mail@gmail.com>
2025-05-12 00:13:39 -0400

    Update GET /api/lessons to filter by slot_id to retrive respective lesson

--- src/app/api/lessons/get.ts
+++ src/app/api/lessons/get.ts
@@ -2,9 +2,13 @@ import { getSession } from "@/auth";
 import { db } from "@/db";
 import { lessons } from "@/db/schema/lessons";
 import { and, eq, SQL } from "drizzle-orm";
+import { NextRequest } from "next/server";
 
-export const GET = async () => {
+export const GET = async (request: NextRequest) => {
   const { userId, role } = await getSession();
+  const { searchParams } = request.nextUrl;
+
+  const slot_id = searchParams.get("slot_id");
 
   const filters: SQL[] = [];
 
@@ -14,6 +18,10 @@ export const GET = async () => {
 
   if (role === "student") {
     filters.push(eq(lessons.student_id, userId));
+  }
+
+  if (slot_id) {
+    filters.push(eq(lessons.slot_id, slot_id));
   }
 
   const res = await db
